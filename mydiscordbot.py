0d6b90db714c7adcb7ba8cd9670e7f571f54fdf8
kaktus087 <44945843+kaktus087@users.noreply.github.com>
2020-05-14 01:40:02 +0300

    Add files via upload

---
+++ mydiscordbot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from sympy import *
+import os
+x = symbols('x')
+
+client = commands.Bot(command_prefix='!')
+
+@client.event
+async def on_ready():
+	#await client.change_presence(status=discord.Status.idle, activity=discord.Game("Ебу алибабу"))
+	print("Bot connected")
+
+@client.command()
+async def bot( ctx, message ):
+	await ctx.send(diff(message))
+
+
+
+TOKEN = os.environ.get('BOT TOKEN')
+client.run(str(TOKEN))
